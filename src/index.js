bb3f287045ec053a69372b875d9ce0e1bdf6c5ea
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-05-17 20:41:27 -0300

    Add ApolloClient to Squire frontend.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
 import App from './App';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
+  })
+});
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={ client }>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
