152506e424c5e7e50064ade155c867ce6070c6f8
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-06-24 17:13:34 -0300

    Turn NoEntries component in a Functional Component.

--- src/components/Timetracker/DailyView/NoEntries.js
+++ src/components/Timetracker/DailyView/NoEntries.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Paper } from 'material-ui';
 
-class NoEntries extends Component {
-  render() {
-    return (
-      <div className="no-entries">
-        <Paper style={ { padding: '20px' } } children="No entries for today" />
-      </div>
-    );
-  }
+function NoEntries(props) {
+  return (
+    <div className="no-entries">
+      <Paper style={ { padding: '20px' } } children="No entries for today" />
+    </div>
+  );
 }
 
 export default NoEntries;
